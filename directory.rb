ee4deec87424c81f66b9fbbdbd1bfb15904eeb1e
Sebastian Lavender <sebastianlavender@outlook.com>
2018-12-18 14:30:47 +0000

    Add load students from students.csv

--- directory.rb
+++ directory.rb
@@ -12,6 +12,8 @@ def interactive_menu
         show_students
       when "3"
         save_students
+      when "4"
+        load_students
       when "9"
         exit #this will cause the program to terminate
       else
@@ -25,6 +27,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -78,6 +81,16 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+#loads the saved students file
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
