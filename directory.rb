ea0696ad41b0f563ea14f567d5d0ea10cbfda852
Sebastian Lavender <sebastianlavender@outlook.com>
2018-12-18 14:22:19 +0000

    Add save students to students.csv

--- directory.rb
+++ directory.rb
@@ -10,6 +10,8 @@ def interactive_menu
         input_students
       when "2"
         show_students
+      when "3"
+        save_students
       when "9"
         exit #this will cause the program to terminate
       else
@@ -22,6 +24,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -64,6 +67,19 @@ end
 #prints footer
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+#saves students to a file
+def save_students
+  #open file for writing
+  file = File.open("students.csv", "w")
+  #iterate over array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 #nothing happens till we run the methods
